b8ff4c797a9933404816965bfc6b108e41bb9112
Mikaia Ralaivita <mikaiajonathan@gmail.com>
2021-10-02 19:44:43 +0200

    refactor: move player detail to its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, PlayersComponent],
+  declarations: [AppComponent, PlayersComponent, PlayerDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/player-detail/player-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Player } from '../players/player.type';
+
+@Component({
+  selector: 'app-player-detail',
+  templateUrl: './player-detail.component.html',
+  styleUrls: ['./player-detail.component.css'],
+})
+export class PlayerDetailComponent implements OnInit {
+  @Input() player?: Player;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
