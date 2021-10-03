758f2333cf2025ca14be0b06447b2ca2adeaeff1
Mikaia Ralaivita <mikaiajonathan@gmail.com>
2021-10-03 19:14:41 +0200

    refactor: Optimize search with rxjs operations

--- src/app/search-player/search-player.component.ts
+++ src/app/search-player/search-player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { PlayerService } from '../player.service';
 import { Player } from '../player.type';
 
@@ -8,15 +10,20 @@ import { Player } from '../player.type';
   styleUrls: ['./search-player.component.css'],
 })
 export class SearchPlayerComponent implements OnInit {
-  players?: Player[];
+  players$!: Observable<Player[]>;
+  searchTerms = new Subject<string>();
 
   constructor(private playerService: PlayerService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.players$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((terms) => this.playerService.search(terms))
+    );
+  }
 
   search(value: string) {
-    this.playerService.search(value.trim()).subscribe((players) => {
-      this.players = players;
-    });
+    this.searchTerms.next(value);
   }
 }
